2fc7ed4dfc4197e76c42d9abdcae3ed8eeb2a7d3
jins <fcipras8694@naver.comm>
2026-04-21 11:35:14 +0900

    second commit

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,24 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// 라우터 설정
+const AuthRouter = require("./router/auth.js");
+
+app.use("/auth", AuthRouter);
+
+// 홈페이지
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ frontend/javascript/signup.js
@@ -0,0 +1,4 @@
+const signup = async (id, password, name) => {
+  const res = await fetch("");
+  const data = await res.json();
+};
